c53f286d79ab543a1858642251cc4412f7bf08cf
Vansh Sachdev <vksvs25@gmail.com>
2024-04-24 12:56:01 +0530

    Error Handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const ExpressError = require('./utils/ExpressError');
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,15 @@ const pricingRouter = require('./routes/pricing');
 const HomeRoute = require('./routes/HomeRoute');
 app.use('/', HomeRoute);
 app.use('/api/pricing', pricingRouter);
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
